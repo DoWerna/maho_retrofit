8a8d0c93d0ceae6636487b27b0f8c6a193693805
DoWerna <lambretta185@web.de>
2021-09-06 11:27:25 +0200

    Öldruckschalter Überwachung hinzugefügt

--- lube.py
+++ lube.py
@@ -3,6 +3,7 @@ import linuxcnc, hal, time
 
 lube = hal.component("lube")
 lube.newpin("fault", hal.HAL_BIT, hal.HAL_OUT)
+lube.newpin("fault2", hal.HAL_BIT, hal.HAL_OUT)
 lube.newpin("run", hal.HAL_BIT, hal.HAL_OUT)
 lube.newpin("delay", hal.HAL_BIT, hal.HAL_OUT)
 lube.newpin("machine_status", hal.HAL_BIT, hal.HAL_IN)
@@ -21,9 +22,12 @@ try:
         #2. spindle needs to be running like in a g-code program
         #3. there should be no pump faults
         if(lube['machine_status'] and lube['spindle_status'] and not lube['fault']):
+            if(lube['pressuresw_floatsw']):
+				lube['fault2'] = 1;
             lube['run'] = 1;
             time.sleep(10) #run pump for 10s
             if(lube['pressuresw_floatsw']):
+                lube['fault2'] = 0;
                 time.sleep(50) #continue running pump for an additional 50s
                 lube['run'] = 0;
                 lube['delay'] = 1; #show that pump is resting
@@ -31,7 +35,7 @@ try:
             else:
                 lube['run'] = 0; #shut off pump immediately
                 lube['fault'] = 1; #there is a fault if input 14 becomes active (fluid low or a big leak somewhere)
-                
+                 
         #gives user to ability to reset the fault after fluid was filled or leak was fixed
         if(lube['reset']):
             lube['fault'] = 0;
